7c2d5ef11c9295106aaab2e07447657e2cd8f63c
nejc <nejc.hocevar8@gmail.com>
2025-04-27 21:46:46 +0200

    Working code from t1r evaluation

--- workspace/build/dis_tutorial3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thelegend442/faxic/RINS-kappa/workspace/src/dis_tutorial3
-# Build directory: /home/thelegend442/faxic/RINS-kappa/workspace/build/dis_tutorial3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- workspace/build/dis_tutorial5/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thelegend442/faxic/RINS-kappa/workspace/src/dis_tutorial5
-# Build directory: /home/thelegend442/faxic/RINS-kappa/workspace/build/dis_tutorial5
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
